ca5c28339e60936ff4a9b111d90bae54eea00a2c
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-04-09 14:45:15 -0700

    added general auth/general log in

--- routers/companies.js
+++ routers/companies.js
@@ -4,24 +4,24 @@ const { auth } = require('../helpers');
 const { companiesHandler } = require('../handlers');
 
 router
-  .route('')
-  .get(auth.ensureCompanyLoggedIn, companiesHandler.readCompanies)
-  .post(companiesHandler.createCompany);
+    .route('')
+    .get(auth.ensureGeneralLoggedIn, companiesHandler.readCompanies)
+    .post(companiesHandler.createCompany);
 
 router.post('/company-auth', companiesHandler.createCompanyToken);
 
 router
-  .route('/:handle')
-  .get(auth.ensureCompanyLoggedIn, companiesHandler.readCompany)
-  .patch(
-    auth.ensureCompanyLoggedIn,
-    auth.ensureCorrectCompany,
-    companiesHandler.updateCompany
-  )
-  .delete(
-    auth.ensureCompanyLoggedIn,
-    auth.ensureCorrectCompany,
-    companiesHandler.deleteCompany
-  );
+    .route('/:handle')
+    .get(auth.ensureCompanyLoggedIn, companiesHandler.readCompany)
+    .patch(
+        auth.ensureCompanyLoggedIn,
+        auth.ensureCorrectCompany,
+        companiesHandler.updateCompany
+    )
+    .delete(
+        auth.ensureCompanyLoggedIn,
+        auth.ensureCorrectCompany,
+        companiesHandler.deleteCompany
+    );
 
 module.exports = router;
