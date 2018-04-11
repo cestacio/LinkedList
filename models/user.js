d64aebdc27bfc78f6ed1985fbd31d595274b816b
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-04-10 17:14:15 -0700

    resolve merge conflict

--- models/user.js
+++ models/user.js
@@ -4,143 +4,149 @@ const validator = new Validator();
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-            firstName: {
-                type: String,
-                minLength: 1,
-                maxLength: 55,
-                required: true
-            },
-            lastName: {
-                type: String,
-                minLength: 1,
-                maxLength: 55,
-                required: true
-            },
-            username: {
-                type: String,
-                minLength: 1,
-                maxLength: 55,
-                required: true,
-                immutable: true
-            },
-            email: {
-                type: String,
-                validate: {
-                    validator: function(validator) {
-                        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(
-                            validator
-                        );
-                    },
-                    message: 'Invalid Email! Try again.'
-                },
-                minLength: 1,
-                maxLength: 55,
-                required: true
-            },
-            password: {
-                type: String,
-                minLength: 1,
-                maxLength: 55,
-                required: true
-            },
-            currentCompanyName: String,
-            currentCompanyId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Company'
-            },
-            photo: String,
-            experience: [{
-                photo: {
-                    type: String,
-                    validate: {
-                        validator: function(validator) {
-                            return /^((https?|ftp):)?\/\/.*(jpeg|jpg|png|gif|bmp)$/.test(
-                                validator
-                            );
-                        },
-                        message: 'URL is not an Image! Try again.'
-                    }
-                },
-                experience: [{
-                    jobTitle: String,
-                    company: String,
-                    startDate: Date,
-                    endDate: Date
-                }],
-                education: [{
-                    institution: String,
-                    degree: String,
-                    endDate: Date
-                }],
-                skills: String
-            }, {
-                timestamps: true
-            });
-
-            userSchema.statics = {
-                createUser(newUser) {
-                    return this.findOne({ username: newUser.username })
-                        .then(username => {
-                            if (username) {
-                                throw new Error(`The username ${newUser.username} exists already.`);
-                            }
-                            return newUser
-                                .save()
-                                .then(user => user)
-                                .catch(err => {
-                                    return Promise.reject(err);
-                                });
-                        })
-                        .catch(err => {
-                            return Promise.reject(err);
-                        });
-                }
-            };
-
-            userSchema.pre('save', function(next) {
-                const user = this;
-
-                if (!user.isModified('password')) {
-                    return next();
-                }
-
-                bcrypt.hash(user.password, 10).then(
-                    hashedPassword => {
-                        user.password = hashedPassword;
-                        return next();
-                    },
-                    err => next(err)
+    firstName: {
+        type: String,
+        minLength: 1,
+        maxLength: 55,
+        required: true
+    },
+    lastName: {
+        type: String,
+        minLength: 1,
+        maxLength: 55,
+        required: true
+    },
+    username: {
+        type: String,
+        minLength: 1,
+        maxLength: 55,
+        required: true,
+        immutable: true
+    },
+    email: {
+        type: String,
+        validate: {
+            validator: function(validator) {
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(
+                    validator
                 );
-            });
+            },
+            message: 'Invalid Email! Try again.'
+        },
+        minLength: 1,
+        maxLength: 55,
+        required: true
+    },
+    password: {
+        type: String,
+        minLength: 1,
+        maxLength: 55,
+        required: true
+    },
+    currentCompanyName: String,
+    currentCompanyId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company'
+    },
+    photo: {
+        type: String,
+        validate: {
+            validator: function(validator) {
+                return /^((https?|ftp):)?\/\/.*(jpeg|jpg|png|gif|bmp)$/.test(
+                    validator
+                );
+            },
+            message: 'URL is not an Image! Try again.'
+        }
+    },
+    experience: [{
+        jobTitle: String,
+        company: String,
+        startDate: Date,
+        endDate: Date
+    }],
+    education: [{
+        institution: String,
+        degree: String,
+        endDate: Date
+    }],
+    skills: String
+}, {
+    timestamps: true
+});
 
-            userSchema.methods.comparePassword = function(candidatePassword, next) {
-                bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-                    if (err) {
-                        return next(err);
-                    }
-                    return next(null, isMatch);
-                });
-            };
-
-            userSchema.post('findOneAndModify', user => {
-                mongoose
-                    .model('Company')
-                    .findOneAndUpdate(user.company, {
-                        $addToSet: { users: user._id }
-                    })
-                    .then(() => {
-                        console.log('POST HOOK RAN');
+userSchema.statics = {
+    createUser(newUser) {
+        return this.findOne({
+                username: newUser.username
+            })
+            .then(username => {
+                if (username) {
+                    throw new Error(`The username ${newUser.username} exists already.`);
+                }
+                return newUser
+                    .save()
+                    .then(user => user)
+                    .catch(err => {
+                        return Promise.reject(err);
                     });
+            })
+            .catch(err => {
+                return Promise.reject(err);
             });
+    }
+};
 
-            userSchema.post('findOneAndRemove', user => {
-                mongoose
-                    .model('Company')
-                    .findOneAndUpdate(user.company, { $pull: { users: user._id } })
-                    .then(() => {
-                        console.log('POST HOOK RAN');
-                    });
-            });
+userSchema.pre('save', function(next) {
+    const user = this;
 
-            const User = mongoose.model('User', userSchema);
-            module.exports = User;
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.hash(user.password, 10).then(
+        hashedPassword => {
+            user.password = hashedPassword;
+            return next();
+        },
+        err => next(err)
+    );
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, next) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        if (err) {
+            return next(err);
+        }
+        return next(null, isMatch);
+    });
+};
+
+userSchema.post('findOneAndModify', user => {
+    mongoose
+        .model('Company')
+        .findOneAndUpdate(user.company, {
+            $addToSet: {
+                users: user._id
+            }
+        })
+        .then(() => {
+            console.log('POST HOOK RAN');
+        });
+});
+
+userSchema.post('findOneAndRemove', user => {
+    mongoose
+        .model('Company')
+        .findOneAndUpdate(user.company, {
+            $pull: {
+                users: user._id
+            }
+        })
+        .then(() => {
+            console.log('POST HOOK RAN');
+        });
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
